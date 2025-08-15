46cdf17994b15812f3ca996aa557c064f8b7453a
Vincent Huang <vincenthuang@mac.mynetworksettings.com>
2025-08-15 12:07:31 -0400

    Fixed code, used ant design input boxes, organized css, style changes

--- ui/app/login/page.js
+++ ui/app/login/page.js
@@ -1,6 +1,8 @@
 "use client";
 import styles from "./page.module.css";
 import { Button } from "antd";
+import { UserOutlined } from '@ant-design/icons';
+import { Input } from "antd";
 import Link from "next/link";
 import React, { useState } from 'react';
 export default function LoginPage() {
@@ -9,31 +11,17 @@ export default function LoginPage() {
     return (
         <div className={styles.pageContent}>
             <h1 className={styles.title}>Login</h1>
-            <h4 className={styles.subTitle}>Enter your information to continue.</h4>
+            <h4 className={styles.subTitle}>Enter your information to continue</h4>
             <div className={styles.formContainer}> 
                 <form className={styles.form}>
                     <div className={styles.inputLabelContainer}>
                         <h4>Email Address</h4>
                     </div>
-                    <input 
-                        className={styles.input}
-                        type="email"
-                        placeholder="Enter email"
-                        value={email}
-                        required
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+                    <Input className={styles.input} size = "large" placeholder="Email" />
                     <div className={styles.inputLabelContainer}>
                         <h4>Password</h4>
                     </div>
-                    <input 
-                    className={styles.input}
-                        onChange={(e) => setPass(e.target.value)}
-                        type="password"
-                        placeholder="Enter password"
-                        value={password}
-                        required
-                    />
+                    <Input className={styles.input}size = "large" placeholder="Password" />
                 </form>
                 <Button color="grey" variant= "solid" type="primary" className={styles.submitButton} >Login</Button>
                     <Link href='/' className={styles.forgotPassword}>Forgot Password?</Link>
