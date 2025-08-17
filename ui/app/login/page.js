29eb7ec0ed96f93411634e5c0a899051e7aebb36
Vincent Huang <huangvincent149@gmail.com>
2025-08-17 14:27:16 -0400

    fixed spacing between labels and text boxes, fixed formatting

--- ui/app/login/page.js
+++ ui/app/login/page.js
@@ -1,34 +1,47 @@
 "use client";
 import styles from "./page.module.css";
 import { Button } from "antd";
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined } from "@ant-design/icons";
 import { Input } from "antd";
 import Link from "next/link";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 export default function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPass] = useState('');
-    return (
-        <div className={styles.pageContent}>
-            <h1 className={styles.title}>Login</h1>
-            <h4 className={styles.subTitle}>Enter your information to continue</h4>
-            <div className={styles.formContainer}> 
-                <form className={styles.form}>
-                    <div className={styles.inputLabelContainer}>
-                        <h4>Email Address</h4>
-                    </div>
-                    <Input className={styles.input} size = "large" placeholder="Email" />
-                    <div className={styles.inputLabelContainer}>
-                        <h4>Password</h4>
-                    </div>
-                    <Input className={styles.input}size = "large" placeholder="Password" />
-                </form>
-                <Button color="grey" variant= "solid" type="primary" className={styles.submitButton} >Login</Button>
-                    <Link href='/' className={styles.forgotPassword}>Forgot Password?</Link>
-                    <div className={styles.signUpContainer}>
-                        <h4 className={styles.signUpTitle}>{ "New to Lunexa?" }</h4> 
-                        <Link href='/' className={styles.createAccount}> Sign up</Link>
-                    </div>
-            </div>
-            </div> )
+  const [email, setEmail] = useState("");
+  const [password, setPass] = useState("");
+  return (
+    <div className={styles.pageContent}>
+      <h1 className={styles.title}>Login</h1>
+      <h4 className={styles.subTitle}>Enter your information to continue</h4>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <div className={styles.inputLabelContainer}>
+            <h4>Email Address</h4>
+          </div>
+          <Input className={styles.input} size="large" placeholder="Email" />
+          <div className={styles.inputLabelContainer}>
+            <h4>Password</h4>
+          </div>
+          <Input className={styles.input} size="large" placeholder="Password" />
+        </form>
+        <Button
+          color="grey"
+          variant="solid"
+          type="primary"
+          className={styles.submitButton}
+        >
+          Login
+        </Button>
+        <Link href="/" className={styles.forgotPassword}>
+          Forgot Password?
+        </Link>
+        <div className={styles.signUpContainer}>
+          <h4 className={styles.signUpTitle}>{"New to Lunexa?"}</h4>
+          <Link href="/" className={styles.createAccount}>
+            {" "}
+            Sign up
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
