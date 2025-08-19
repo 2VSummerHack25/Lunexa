093d1538a08c1d1013a1ecf7625c15ac8884934a
Jesse Najera <jessenajera06@gmail.com>
2025-08-19 19:45:45 -0400

    made component dynamic, takes in url, userName, userRole, matchReason

--- ui/app/components/userInfo.js
+++ ui/app/components/userInfo.js
@@ -5,33 +5,69 @@ import { Button } from "antd";
 {
 }
 
-export default function UserInfo() {
-  return (
-    <div className={styles.card}>
-      <div className={styles.cardTop}>
-        <ProfilePic
-          url="https://pyxis.nymag.com/v1/imgs/5ff/9bc/6098962edd260c49d52d4b2b58d4df0b62-13-miles-morales-lede.rsquare.w400.jpg"
-          size="small"
-        />
-        <div className={styles.userInfo}>
-          <h2>Miles Morales</h2>
-          <h4 className={styles.subtitle}>Design Engineer</h4>
-        </div>
-        <div className={styles.buttonContainer}>
-          <div className={styles.topButtons}>
-            <Button type="primary" shape="circle" icon={<MailOutlined />} />
-            <Button type="primary" shape="circle" icon={<EllipsisOutlined />} />
+export default function UserInfo({ url, userName, userRole, matchReason }) {
+  if (url && userName && userRole && matchReason) {
+    return (
+      <div className={styles.card}>
+        <div className={styles.cardTop}>
+          <ProfilePic url={url} size="small" />
+          <div className={styles.userInfo}>
+            <h2>{userName}</h2>
+            <h4 className={styles.subtitle}>{userRole}</h4>
           </div>
-          <Button className={styles.meetingButton} type="primary">
-            Set up meeting
-          </Button>
+          <div className={styles.buttonContainer}>
+            <div className={styles.topButtons}>
+              <Button type="primary" shape="circle" icon={<MailOutlined />} />
+              <Button
+                type="primary"
+                shape="circle"
+                icon={<EllipsisOutlined />}
+              />
+            </div>
+            <Button className={styles.meetingButton} type="primary">
+              Set up meeting
+            </Button>
+          </div>
+        </div>
+        <div className={styles.divider} />
+        <div className={styles.cardBottom}>
+          <h4 className={styles.subtitle}>Match Reason</h4>
+          <h3>{matchReason}</h3>
         </div>
       </div>
-      <div className={styles.divider} />
-      <div className={styles.cardBottom}>
-        <h4 className={styles.subtitle}>Match Reason</h4>
-        <h3>Mentorship</h3>
+    );
+  } else {
+    return (
+      <div className={styles.card}>
+        <div className={styles.cardTop}>
+          <ProfilePic
+            url="https://pyxis.nymag.com/v1/imgs/5ff/9bc/6098962edd260c49d52d4b2b58d4df0b62-13-miles-morales-lede.rsquare.w400.jpg"
+            size="small"
+          />
+          <div className={styles.userInfo}>
+            <h2>Miles Morales</h2>
+            <h4 className={styles.subtitle}>Design Engineer</h4>
+          </div>
+          <div className={styles.buttonContainer}>
+            <div className={styles.topButtons}>
+              <Button type="primary" shape="circle" icon={<MailOutlined />} />
+              <Button
+                type="primary"
+                shape="circle"
+                icon={<EllipsisOutlined />}
+              />
+            </div>
+            <Button className={styles.meetingButton} type="primary">
+              Set up meeting
+            </Button>
+          </div>
+        </div>
+        <div className={styles.divider} />
+        <div className={styles.cardBottom}>
+          <h4 className={styles.subtitle}>Match Reason</h4>
+          <h3>{matchReason}</h3>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
